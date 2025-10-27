584688faafc55ffd23bdb513f2be94ee1efbe8eb
IncognicatJess <jaisyunurm@gmail.com>
2025-10-27 21:11:26 +0700

    newewst

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/pomofocus-clone/", // <-- ganti sesuai nama repo GitHub
   plugins: [react()],
+  base: "/pomofocus-clone/",
 });
