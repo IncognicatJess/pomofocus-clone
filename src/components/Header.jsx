1fcc0f3365a6a40e5bd5b3374164a0199fc4d027
IncognicatJess <jaisyunurm@gmail.com>
2025-10-27 21:21:19 +0700

    Fix case-sensitive file name issue

--- src/components/Header.jsx
+++
@@ -1,3 +0,0 @@
-export default function Header() {
-  return <h1>Pomofocus Clone</h1>;
-}
--- src/components/SettingsModal.jsx
+++
@@ -1,7 +0,0 @@
-export default function SettingsModal() {
-  return (
-    <footer style={{ marginTop: "2rem", opacity: 0.6 }}>
-      <small>⚙️ Settings (Coming Soon)</small>
-    </footer>
-  );
-}
